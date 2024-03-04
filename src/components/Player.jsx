90136d3aef6664b3c6b991bd5aac2d6a466f0b54
Uthaya-Uitoux <uthaya@uitoux.in>
2024-03-04 17:47:03 +0530

    basic code created.worked till name displaying on the save and edit option.

---
+++ src/components/Player.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+
+export default function Player({initialName,symbol}){
+    const[playerName, setPlayerName]= useState(initialName);
+    const [isEditing, setIsEditing]= useState(false);
+    
+    function handleEdit(){
+       // setIsEditing(isEditing ? false : true); - -one method of having the opp statement
+    /* setIsEditing(!isEditing);//Easy method to change the previous value - in repeating this code does not give the opp value to the previous value.
+     setIsEditing(!isEditing);//Both line gives the true value*/
+     //3rd method
+    setIsEditing(editing=>!editing);//easy method tochange the value using prev state
+    }
+    function handleChange(event){
+        setPlayerName(event.target.value);
+    }
+
+      let editablePlayerName = <span className='player-name'>{playerName}</span>;
+      //let btnCaption='Edit';
+      if(isEditing){
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
+       //btnCaption='Save';
+      }
+
+    return(
+        <li>
+        <span className='player'>
+        {editablePlayerName}
+        <span className='player-symbol'>{symbol}</span>
+        <button onClick={handleEdit}>{isEditing ? 'Save' : 'Edit'}</button>
+        </span>
+        </li>
+    )
+}
